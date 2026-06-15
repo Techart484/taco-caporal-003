3881acc3aecc4450a01387e942c556b64eb3291c
Techart484 <Irankin89@outlook.com>
2026-06-14 22:52:30 -0400

    feat(awde): ship app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Taco Caporal",
+  description: "Taco Caporal offers a vibrant taste of Mexico through authentic, fresh, and flavorful tacos and traditional dishes, providing a convenient and satisfying dining experience.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
